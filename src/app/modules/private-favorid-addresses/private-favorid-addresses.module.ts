683b2e202b8b32c3cb0d57a6efdfcf960cd49dea
Ali Dehghani <alixdehghani@gmail.com>
2022-10-19 17:50:41 +0330

    fix some minor bugs

--- src/app/modules/private-favorid-addresses/private-favorid-addresses.module.ts
+++ src/app/modules/private-favorid-addresses/private-favorid-addresses.module.ts
@@ -1,9 +1,18 @@
+import { HTTP_INTERCEPTORS } from "@angular/common/http";
 import { NgModule } from "@angular/core";
+import { AuthInterceptor } from "src/app/auth/auth.interceptor";
 import { PrivateFavoridAddressesComponent } from "./private-favorid-addresses.component";
 
 @NgModule({
     declarations: [PrivateFavoridAddressesComponent],
     imports: [],
-    exports: []
+    exports: [],
+    providers: [
+        {
+            provide: HTTP_INTERCEPTORS,
+            useClass: AuthInterceptor,
+            multi: true
+        }
+    ]
 })
-export class PrivateFavoridAddressesModule {}
+export class PrivateFavoridAddressesModule { }
